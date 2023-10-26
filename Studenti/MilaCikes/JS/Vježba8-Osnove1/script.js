738a4671ec907cf2898dac2bcdf34dc52170a90e
MilaCikes <138057364+MilaCikes@users.noreply.github.com>
2023-10-26 20:41:10 +0200

    Vjezba8

---
+++ Studenti/MilaCikes/JS/Vježba8-Osnove1/script.js
@@ -0,0 +1,22 @@
+var firstName = "Ivan";
+var lastName = "Horvat";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// 2.
+
+// var x = z == 2 ? y : 5;
+var z = 2;
+var y = 10;
+
+var x;
+
+if (z === 2) {
+  x = y;
+} else {
+  x = 5;
+}
+
+console.log("x je:" + 10);
