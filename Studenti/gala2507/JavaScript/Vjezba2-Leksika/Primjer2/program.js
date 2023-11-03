b118970ead3c64c8366558d984043b4e927d40b4
gala2507 <gala2507@gmail.com>
2023-11-03 20:18:16 +0100

    leksika

--- Studenti/gala2507/JavaScript/Vjezba2-Leksika/Primjer2/program.js
+++ Studenti/gala2507/JavaScript/Vjezba2-Leksika/Primjer2/program.js
@@ -10,7 +10,7 @@ function createTable(rows, cols) {
   var j = 1;
   var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
 
-  for (i = 1; i <= rows; i++) {
+  for (var i = 1; i <= rows; i++) {
     output = output + "<tr>";
     while (j <= cols) {
       output = output + "<td>" + i * j + "</td>";
