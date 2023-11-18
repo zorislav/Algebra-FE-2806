9c002a8d5f0abb2fe4e139f047494ecfbbb141c0
MilaCikes <138057364+MilaCikes@users.noreply.github.com>
2023-11-18 22:33:09 +0100

    Vježba15

--- Studenti/MilaCikes/JS/Vjezba15/script.js
+++ Studenti/MilaCikes/JS/Vjezba15/script.js
@@ -1,6 +1,23 @@
-//var mojdive1 =  document.getElementById("mojdiv");
-
-//console.log(mojdive1.innerText)
+// 1
 
 var h1 = document.querySelector("h1");
 h1.innerText = "Karlovac";
+
+// 2
+
+var description = document.querySelector(".description");
+description.innerHTML = "Grad na <h3>5 rijeka</h3>";
+
+// 3
+var info = document.querySelector(".info");
+info.innerText = "Peta je pivo";
+
+// 4
+
+var rijeke = ["Mrežnica", "Kupa", "Korana", "Dobra", "Karlovačko"];
+
+var footer = document.querySelector("#footer");
+
+for (var i = 0; i < rijeke.length; i++) {
+  footer.querySelectorAll("li")[i].innerText = rijeke[i];
+}
