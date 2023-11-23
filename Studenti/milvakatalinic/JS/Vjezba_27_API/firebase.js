8320dcd563e5cb344c046314924b5dff076736ae
milvakatalinic <milva.katalinic@gmail.com>
2023-11-23 13:42:42 +0100

    Commit
    vjezbe 27-29

---
+++ Studenti/milvakatalinic/JS/Vjezba_27_API/firebase.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById("input");
+const ulEl = document.getElementById("lista");
+const posaljiButtonEl = document.getElementById("posalji");
+const prpcitajButtonEl = document.getElementById("procitaj");
+
+async function getData() {}
+
+procitajButtonEl.addEventListener("click", getData);
---
+++ Studenti/milvakatalinic/JS/Vjezba_29_TODO1/script.js
@@ -0,0 +1,11 @@
+//function Todo() {
+
+//};
+
+//Todo.prototype.init = function() {
+//inicijalizacija event listenera
+//}
+
+//var todo = new Todo();
+
+//window.addEventListener('load', todo.init());
