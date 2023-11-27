7b8cfcf3150012f0954dee1ed138634781f92948
Zori <46168195+zorislav@users.noreply.github.com>
2023-11-27 16:27:49 +0100

    Add files via upload

---
+++ JavaScript/Parcijalni ispit/src/data.js
@@ -0,0 +1,25 @@
+export const fetchData = async (inputElValue) => {
+  
+  // Kreiraj url za pretrazivanje
+  const url = `https://itunes.apple.com/search?term=${inputElValue}&media=music`;
+
+  try {
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const returnData = data.results.map(result => {
+      return {
+        artist: result.artistName,
+        song: result.trackName,
+      }
+    });
+
+    return returnData;
+  
+  } catch (error) {
+
+    // U slucaju greske vrati prazan niz
+    return [];
+  }
+
+}
