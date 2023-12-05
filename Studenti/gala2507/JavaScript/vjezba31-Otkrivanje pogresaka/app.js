61c9d9adf9251a0464ac784e7487c8f683753e0c
gala2507 <gala2507@gmail.com>
2023-12-05 18:40:56 +0100

    otkrivanje

---
+++ Studenti/gala2507/JavaScript/vjezba31-Otkrivanje pogresaka/app.js
@@ -0,0 +1,9 @@
+/*jslint single devel */
+
+const name1 = "James";
+
+const person = { first: name1 };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => console.log(`Hello linting, ${fName}`);
