19c9a56aa764d408743eba9870cfbff52833b91a
gala2507 <gala2507@gmail.com>
2024-01-01 17:04:03 +0100

    vjezbe 3,4,5 7

---
+++ Studenti/gala2507/React/Vjezba7-Komponente/komponente/src/WelcomeClass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class WelcomeClass extends React.Component {
+  constructor() {
+    super();
+
+    //...inicijalizacija
+  }
+
+  render() {
+    return <h2>Welcome! Komponenta definirana klasom</h2>;
+  }
+}
