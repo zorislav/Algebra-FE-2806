5353ec2a4091e77ac8d98744abdec3d7ee4d2a7e
Zori <46168195+zorislav@users.noreply.github.com>
2024-01-08 10:02:34 +0100

    Add files via upload

---
+++ React/Vjezba 16 - Context/src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { Provider } from './context/context';
+import Komponenta1 from './components/Komponenta1';
+import './App.css';
+
+function App() {
+
+  const [number, setNumber] = useState(0);
+
+  function textChangeHandler(event){
+    setNumber(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <Provider value={{text: 'Komponenta 4 je najbolja', number}}>
+        <label>
+          <input type="number" value={number} onChange={textChangeHandler} />
+        </label>
+        <Komponenta1 />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
