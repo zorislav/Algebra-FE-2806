f04b9270f69b497836dc4fa2962160410f5e6573
gala2507 <gala2507@gmail.com>
2024-01-10 18:02:06 +0100

    obrasci

---
+++ Studenti/gala2507/React/Vjezba15-Obrasci2/obrasci2/src/user/UserFunction.js
@@ -0,0 +1,12 @@
+function UserFunction({ ime, years, onNameChange }) {
+  return (
+    <>
+      <p>
+        Pozdrav, moje ime je {ime} i imam {years} godina
+      </p>
+      <input type="text" onChange={onNameChange} />
+    </>
+  );
+}
+
+export default UserFunction;
