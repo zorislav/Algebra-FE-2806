3a877a352d602abc8add05bb7416f153c79802c5
gala2507 <gala2507@gmail.com>
2024-01-12 18:15:47 +0100

    vjezba 16 i 17

---
+++ Studenti/gala2507/React/Vjezba16-Context/context/src/components/Komponenta5.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Context, { Consumer } from "../context/context";
+
+class Komponenta5 extends React.Component{
+
+  static contextType = Context;
+
+  render(){
+
+    const { text, number } = this.context;
+    const editText = `${text} preko funkcije, a poslani broj je: ${number}`;
+
+    return(
+      <>
+        <h2>Komponenta5</h2>
+        <Consumer>{ctx => <p>Komponenta 5 ispisuje: {ctx.text}</p>}</Consumer>
+        <p>{editText}</p>
+      </>
+    );
+  }    
+}
+
+
+export default Komponenta5;
