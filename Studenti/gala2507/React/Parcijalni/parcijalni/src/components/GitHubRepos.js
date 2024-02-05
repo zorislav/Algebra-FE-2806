4c487f7de8caccfecf3bd3cac24a3c950d9d7b2f
gala2507 <gala2507@gmail.com>
2024-02-05 17:32:15 +0100

    parcijalni

---
+++ Studenti/gala2507/React/Parcijalni/parcijalni/src/components/GitHubRepos.js
@@ -0,0 +1,26 @@
+function GithubRepos({ repos }) {
+  if (!repos || repos.length <= 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>REPOSITORIES: </strong>
+      </p>
+      <ul style={styles.ul}>
+        {repos.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default GithubRepos;
+
+const styles = {
+  ul: {
+    listStyleType: "none",
+  },
+};
---
+++ Studenti/gala2507/React/Parcijalni/parcijalni/src/services/index.js
@@ -0,0 +1,1 @@
+export { default as GitHubApi } from "./GitHubApi";
